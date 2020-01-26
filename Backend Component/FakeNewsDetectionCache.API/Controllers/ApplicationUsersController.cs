7b1c434071481db1156177c9a87962d714df203b
Munteanu Andrei-Stefan <munteanu.stefan@outlook.com>
2020-01-26 18:44:18 +0200

    API controllers

---
+++ Backend Component/FakeNewsDetectionCache.API/Controllers/ApplicationUsersController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FakeNewsDetectionCache.API.ViewModels.ApplicationUser;
+using FakeNewsDetectionCache.Authentication.Authorization;
+using FakeNewsDetectionCache.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FakeNewsDetectionCache.API.Controllers
+{
+    //[Log]
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUsersController : ControllerBase
+    {
+        protected IApplicationUserService ApplicationUserService;
+
+        public ApplicationUsersController(IApplicationUserService applicationUserService)
+        {
+            ApplicationUserService = applicationUserService;
+        }
+
+        [HttpGet]
+        [Authorize(Policy = Policies.OnlyExtension)]
+        public async Task<JsonResult> Get()
+        {
+            var items = await ApplicationUserService.GetAll();
+            if (items.Count == 0)
+                Response.StatusCode = StatusCodes.Status404NotFound;
+            return new JsonResult(items);
+        }
+
+        [HttpPost("{model}")]
+        [Authorize(Policy = Policies.OnlyExtension)]
+        public async Task<JsonResult> GetFiltered(ApplicationUserFilterViewModel model)
+        {
+            var items = await model.ApplyFilter(await ApplicationUserService.GetAsQueriable());
+            if (items.Count == 0)
+                Response.StatusCode = StatusCodes.Status404NotFound;
+            return new JsonResult(items);
+        }
+
+        [HttpPost]
+        [Authorize(Policy = Policies.OnlyDevelopers)]
+        public async Task Post(ApplicationUserViewModel model)
+        {
+            await ApplicationUserService.Add(model.ToEntity());
+        }
+
+        [HttpPut]
+        [Authorize(Policy = Policies.OnlyDevelopers)]
+        public async Task Put(ApplicationUserViewModel model)
+        {
+            try
+            {
+                await ApplicationUserService.Update(model.ToEntity());
+            }
+            catch { Response.StatusCode = StatusCodes.Status400BadRequest; }
+        }
+
+        [HttpDelete("{Id}")]
+        [Authorize(Policy = Policies.OnlyDevelopers)]
+        public async Task Delete(Guid Id)
+        {
+            var entityToDelete = (await ApplicationUserService.GetByFilter(x => x.Id == Id)).FirstOrDefault();
+
+            if (entityToDelete != null)
+                await ApplicationUserService.Delete(entityToDelete);
+            else
+                Response.StatusCode = StatusCodes.Status404NotFound;
+        }
+    }
+}
---
+++ Backend Component/FakeNewsDetectionCache.API/Startup.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FakeNewsDetectionCache.API.Swagger;
+using FakeNewsDetectionCache.Authentication;
+using FakeNewsDetectionCache.Authentication.Authorization;
+using FakeNewsDetectionCache.Authentication.Authorization.Handlers;
+using FakeNewsDetectionCache.Authentication.Authorization.Requirements;
+using FakeNewsDetectionCache.Entities;
+using FakeNewsDetectionCache.Entities.Models;
+using FakeNewsDetectionCache.Interfaces;
+using FakeNewsDetectionCache.Service;
+using FakeNewsDetectionCache.Service.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace FakeNewsDetectionCache.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            ConfigureAuthentication(services);
+
+            ConfigureAuthorization(services);
+
+
+            ConfigureContext(services);
+
+            ConfigureEntitiesServices(services);
+
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            ConfigureSwagger(services);
+
+            services.Configure<ProcessingUnitConfig>(Configuration.GetSection("ProcessingUnitConfig"));
+
+        }
+
+        public void ConfigureAuthentication(IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = ApiKeyAuthenticationOptions.DefaultScheme;
+                options.DefaultChallengeScheme = ApiKeyAuthenticationOptions.DefaultScheme;
+            })
+                    .AddApiKeySupport(options => { });
+
+            services.AddScoped<IGetApiKeyQuery, InMemoryGetApiKeyQuery>();
+        }
+
+        public void ConfigureAuthorization(IServiceCollection services)
+        {
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(Policies.OnlyExtension, policy => policy.Requirements.Add(new OnlyExtensionRequirement()));
+                options.AddPolicy(Policies.OnlyDevelopers, policy => policy.Requirements.Add(new OnlyDevelopersRequirement()));
+                options.AddPolicy(Policies.OnlyProcessingService, policy => policy.Requirements.Add(new OnlyProcessingServiceRequirement()));
+            });
+
+            services.AddSingleton<IAuthorizationHandler, OnlyExtensionAuthorizationHandler>();
+            services.AddSingleton<IAuthorizationHandler, OnlyDevelopersAuthorizationHandler>();
+            services.AddSingleton<IAuthorizationHandler, OnlyProcessingServiceAuthorizationHandler>();
+        }
+
+        public void ConfigureContext(IServiceCollection services)
+        {
+            //var connection = @"Server=localhost\SQLEXPRESS;Database=FakeNewsDetectionCache;Trusted_Connection=True;";
+            var connection =
+            Microsoft
+                .Extensions
+                .Configuration
+                .ConfigurationExtensions
+                .GetConnectionString(this.Configuration, "FakeNewsModelContainer");
+
+
+            services.AddDbContext<Repository>(options => options.UseSqlServer(connection)
+                                                        .UseLoggerFactory(new LoggerFactory().AddFile("Logs/ts-{Date}.txt"))
+                                                        .EnableSensitiveDataLogging()
+                                                        );
+
+            //services.AddDbContext<Repository>(options => options.UseInMemoryDatabase("fakedb")
+            //.UseLoggerFactory(new LoggerFactory().AddFile("LogsInMemory/ts-{Date}.txt"))
+            //                                            .EnableSensitiveDataLogging()
+            //                                            );
+        }
+
+        public void ConfigureEntitiesServices(IServiceCollection services)
+        {
+            services.AddScoped<ITwitterUserService, TwitterUserService>();
+            services.AddScoped<IApplicationUserService, ApplicationUserService>();
+            services.AddScoped<INewsArticleService, NewsArticleService>();
+            services.AddScoped<IVoteService, VoteService>();
+        }
+
+
+        public void ConfigureSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                c.OperationFilter<AddRequiredHeaderParameter>();
+            });
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fake News Detection Cache API V1");
+                //c.RoutePrefix = string.Empty;
+            });
+
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+
+
+        }
+    }
+}
